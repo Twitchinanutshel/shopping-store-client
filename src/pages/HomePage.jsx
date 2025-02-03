94ade96191994e6667c0b681840a1326dc294967
Noah Gauci <breakf117@gmail.com>
2025-02-04 07:59:33 +1100

    fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -21,11 +21,11 @@ const HomePage = () => {
       <div className='flex flex-col items-center'>
         <h1 className='font-semibold text-2xl'>Welcome!</h1>
         <Link className='mt-3 bg-gray-500 text-white px-6 py-2 rounded hover:bg-gray-600' to='/shopping-items'>Click here to go shopping!</Link>
-        {backend ? (
+        {backend ? null : (
           <>
             <h1 className='bg-red-800 px-6 py-2'>HEROKU BACKEND IS NOT RUNNING!</h1>
           </>
-        ) : null}
+        )}
       </div>
     </>
   )
